21c3b5c860c66ddb0bc84310de49ee5c0a62c893
mbish08 <mdrew8@gmail.com>
2021-05-26 18:39:59 -0700

    commit tree 3fc8b60e810616bbef4af1de31dbaf41cd252664

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,38 @@
-export function manageFriends(state, action){
+export function manageFriends(state = {friends: []}, action){
+    switch (action.type) {
+        case "ADD_FRIEND":
+      return (
+        {...state,
+            friends: [
+              ...state.friends,
+              action.friend
+            ]
+        }
+    )
+
+    case "REMOVE_FRIEND":
+      const removalIndex = state.friends.findIndex(friend => friend.id === action.id);
+      return (
+        {...state,
+            friends: [
+              ...state.friends.slice(0, removalIndex),
+              ...state.friends.slice(removalIndex + 1)
+            ]
+        }
+      )
+
+    default:
+      return state;
+    }
 }
+
+// function reducer(state, action){      
+//     switch (action.type) {
+//       case 'INCREASE_COUNT':
+//         return {count: state.count + 1}
+//       case 'DECREASE_COUNT':
+//         return {count: state.count - 1}
+//       default:
+//         return state;
+//     }
+//   } 
